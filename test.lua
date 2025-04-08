82180eb898ea85826388cd655dc2376ce3f285cd
nola <Nola.Bloop@gmail.com>
2025-04-07 21:35:21 -0400

    circular list stable

--- test.lua
+++ test.lua
@@ -1,3 +1,22 @@
-require("CircularList")
+local CircularList = require("CircularList")
 
 local list = CircularList.new()
+
+list:insert("3")
+list:insert("7") --x
+list:insert("2")
+list:insert("8")
+list:insert("9")
+
+list:next(1)
+list:remove()
+list:next(2)
+list:insert("peepoo")
+
+
+
+list:setFlag(true)
+print(list:next())
+while not list:readFlag() do
+    print(list:next())
+end
